eaacb72edbdf81d758f585f2635d53fff2a17688
Jethro-Mush <mushilingwaj@gmail.com>
2023-05-30 12:50:55 -0700

    changes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,10 +4,11 @@ class Square:
     """
     def __init__(self, side_length):
         self.side_length = side_length
-
+    """Initializes an instance variable
+    """
     def area(self):
         return self.side_length ** 2
-
+    """to calculate the permitere of the square
+    """
     def perimeter(self):
         return 4 * self.side_length
-
