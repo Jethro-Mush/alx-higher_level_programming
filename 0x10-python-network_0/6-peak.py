04fee6ee009ddedd9857f95a326d43478843400d
Jethro-Mush <mushilingwaj@gmail.com>
2023-08-31 05:19:13 -0700

    changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-""" Finds a peak inside a list """
-
-
-def find_peak(list_of_integers):
-    if list_of_integers == []:
+""" Test function find_peak """
+    def find_peak(list_of_integers):
+    if not list_of_integers:
         return None
-
-    length = len(list_of_integers)
-    mid = int(length / 2)
-    li = list_of_integers
-
-    if mid - 1 < 0 and mid + 1 >= length:
-        return li[mid]
-    elif mid - 1 < 0:
-        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= length:
-        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
-
-    if li[mid - 1] < li[mid] > li[mid + 1]:
-        return li[mid]
-
-    if li[mid + 1] > li[mid - 1]:
-        return find_peak(li[mid:])
-    return find_peak(li[:mid])
+    
+    low = 0
+    high = len(list_of_integers) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+        
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+            
+    return list_of_integers[low]
